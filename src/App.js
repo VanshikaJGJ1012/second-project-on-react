85c3552193f7f9083ebe44576a8ea1efc4a7e285
Vanshika <vanshipahwa99@gmail.com>
2021-09-01 21:03:50 +0530

    Introduction to Redux

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import Main from './components/MainComponent';
 import './App.css';
 
 import { BorwserRouter } from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {ConfigureStore} from './redux/configureStore';
 
 
 class App extends Component {
   render() {
     return (
+      <Provider store={store}>
       <BorwserRouter>
       <div className="App">
         
         <Main />
       </div>
       </BorwserRouter>
+      </Provider>
     );
   }
 }
