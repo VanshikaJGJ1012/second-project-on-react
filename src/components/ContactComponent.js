ed67a7842143c8a9a2515df1fdd5a1f4c214e0c3
Vanshika <vanshipahwa99@gmail.com>
2021-09-01 15:39:54 +0530

    Controlled Forms

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,53 +1,227 @@
-import React from 'react';
-import {Breadcrumb,BreadcrumbItem} from 'reactstrap';
-import {Link} from 'react-router-dom';
-function Contact(props) {
-    return(
-        <div className="container">
-             <div className="row">
-        <BreadCrumb>
-        <BreadCrumbItem>
-        <Link to ='/home'>Home
-        </Link>
-        </BreadCrumbItem>
-        <BreadCrumbItem active>Contact Us</BreadCrumbItem>
-        </BreadCrumb>
-        <div className="col-12">
-<h3>
-  Contact us
+import React, { Component } from "react";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Col,
+} from "reactstrap";
+import { Link } from "react-router-dom";
+class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstname: "",
+      lastname: "",
+      telnum: "",
+      email: "",
+      agree: false,
+      contactType: "Tel.",
+      message: "",
+    };
+    this.handleSubmit=this.handleSubmit.bind(this);
+    this.handleInputChange=this.handleInputChange.bind(this);
+  }
+  handleInputChange(event){
+ const target=event.target;
+ const value=target.type==="checkbox"?target.checked:target.value;
+ const name=target.name;
+ this.setState({
+     [name]:value
+ });
+  }
+  handleSubmit(event){
+console.log("Current State is:"+JSON.stringify(this.state));
+alert("Current State is:"+JSON.stringify(this.state));
+event.preventDefault();
 
-</h3>
-<hr/>
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <BreadCrumb>
+            <BreadCrumbItem>
+              <Link to="/home">Home</Link>
+            </BreadCrumbItem>
+            <BreadCrumbItem active>Contact Us</BreadCrumbItem>
+          </BreadCrumb>
+          <div className="col-12">
+            <h3>Contact us</h3>
+            <hr />
+          </div>
+        </div>
+        <div className="row row-content">
+          <div className="col-12">
+            <h3>Location Information</h3>
+          </div>
+          <div className="col-12 col-sm-4 offset-sm-1">
+            <h5>Our Address</h5>
+            <address>
+              121, Clear Water Bay Road
+              <br />
+              Clear Water Bay, Kowloon
+              <br />
+              HONG KONG
+              <br />
+              <i className="fa fa-phone"></i>: +852 1234 5678
+              <br />
+              <i className="fa fa-fax"></i>: +852 8765 4321
+              <br />
+              <i className="fa fa-envelope"></i>:{" "}
+              <a href="mailto:confusion@food.net">confusion@food.net</a>
+            </address>
+          </div>
+          <div className="col-12 col-sm-6 offset-sm-1">
+            <h5>Map of our Location</h5>
+          </div>
+          <div className="col-12 col-sm-11 offset-sm-1">
+            <div className="btn-group" role="group">
+              <a
+                role="button"
+                className="btn btn-primary"
+                href="tel:+85212345678"
+              >
+                <i className="fa fa-phone"></i> Call
+              </a>
+              <a role="button" className="btn btn-info">
+                <i className="fa fa-skype"></i> Skype
+              </a>
+              <a
+                role="button"
+                className="btn btn-success"
+                href="mailto:confusion@food.net"
+              >
+                <i className="fa fa-envelope-o"></i> Email
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="row row-content">
+          <div className="col-12">
+            <h3>Send Us Your Feedback</h3>
+          </div>
+          <div className="col-12 col-md-9">
+            <Form onSubmit={this.handleSubmit}>
+              <FormGroup row>
+                <label htmlFor="firstname" md={2}>
+                  First Names
+                </label>
+                <col md={10}>
+                  <input
+                    type="text"
+                    id="firstname"
+                    name="firstname"
+                    placeholder="First Name"
+                    value={this.state.firstname}
+                    onChange={this.handleInputChange}
+                  />
+                </col>
+              </FormGroup>
+
+              <FormGroup row>
+                <label htmlFor="lastname" md={2}>
+                  Last Names
+                </label>
+                <col md={10}>
+                  <input
+                    type="text"
+                    id="lastname"
+                    name="lastname"
+                    placeholder="Last Name"
+                    value={this.state.lastname}
+                    onChange={this.handleInputChange}
+                  />
+                </col>
+              </FormGroup>
+
+              <FormGroup row>
+                <label htmlFor="telnum" md={2}>
+                  Contact Tel.
+                </label>
+                <col md={10}>
+                  <input
+                    type="tel"
+                    id="telnum"
+                    name="telnum"
+                    placeholder="Tel. Number"
+                    value={this.state.telnum}
+                    onChange={this.handleInputChange}
+                  />
+                </col>
+              </FormGroup>
+
+              <FormGroup row>
+                <label htmlFor="email" md={2}>
+                  Email
+                </label>
+                <col md={10}>
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="Email"
+                    value={this.state.email}
+                    onChange={this.handleInputChange} 
+                  />
+                </col>
+              </FormGroup>
+              <FormGroup row>
+                <Col md={{ size: 6, offset: 2 }}>
+                  <FormGroup check>
+                    <Label check>
+                      <input
+                        type="checkbox"
+                        name="agree"
+                        checked={this.state.agree}
+                        onChange={this.handleInputChange}
+                      />{" "}
+                      <strong>May We Contact You?</strong>
+                    </Label>
+                  </FormGroup>
+                </Col>
+              </FormGroup>
+              <Col md={{size:3,offset:1}}>
+                  <input type="select" name="contactType" value={this.state.contactType}>
+                  onChange={this.handleInputChange}
+                  <option>Tel.</option>
+                  <option>Email</option>    
+                  </input>
+              </Col>
+
+              <FormGroup row>
+                <label htmlFor="message" md={2}>
+                 Your Feedback
+                </label>
+                <col md={10}>
+                  <input
+                    type="textarea"
+                    id="message"
+                    name="message"
+                   rows="12"
+                    value={this.state.message}
+                    onChange={this.handleInputChange}
+                  />
+                </col>
+              </FormGroup>
+
+
+              <FormGroup row>
+                  <Col md={{size:10,offset:2}}>
+                      <Button type="submit" color="primary">
+                          Send Feedback
+                          </Button> 
+                  </Col>
+              </FormGroup>
+            </Form>
+          </div>
         </div>
       </div>
-            <div className="row row-content">
-                <div className="col-12">
-                <h3>Location Information</h3>
-                </div>
-                <div className="col-12 col-sm-4 offset-sm-1">
-                        <h5>Our Address</h5>
-                        <address>
-                        121, Clear Water Bay Road<br />
-                        Clear Water Bay, Kowloon<br />
-                        HONG KONG<br />
-                        <i className="fa fa-phone"></i>: +852 1234 5678<br />
-                        <i className="fa fa-fax"></i>: +852 8765 4321<br />
-                        <i className="fa fa-envelope"></i>: <a href="mailto:confusion@food.net">confusion@food.net</a>
-                        </address>
-                </div>
-                <div className="col-12 col-sm-6 offset-sm-1">
-                    <h5>Map of our Location</h5>
-                </div>
-                <div className="col-12 col-sm-11 offset-sm-1">
-                    <div className="btn-group" role="group">
-                        <a role="button" className="btn btn-primary" href="tel:+85212345678"><i className="fa fa-phone"></i> Call</a>
-                        <a role="button" className="btn btn-info"><i className="fa fa-skype"></i> Skype</a>
-                        <a role="button" className="btn btn-success" href="mailto:confusion@food.net"><i className="fa fa-envelope-o"></i> Email</a>
-                    </div>
-                </div>
-            </div>
-        </div>
     );
+  }
 }
 
 export default Contact;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,71 +1,93 @@
 import React, { Component } from 'react';
 
-
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
-import './App.css';
-import { DISHES } from '../shared/dishes';
-import { PROMOTIONS } from '../shared/promotions';
-import { COMMENTS} from '../shared/comments';
-import { LEADERS } from '../shared/leaders';
-import DishDetail from './DishDetailComponent';
-import Header from './HomeComponent';
+import About from './AboutComponent';
+import Contact from './ContactComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
-import Contact from './ContactComponent';
+import DishDetail from './DishdetailComponent';
 
-import {Switch,Route,Redirect} from 'react-router-dom';
-import Home from './HomeComponent';
+import { COMMENTS } from '../shared/comments'
+import { DISHES } from '../shared/dishes'
+import { LEADERS } from '../shared/leaders'
+import { PROMOTIONS } from '../shared/promotions'
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+import { Switch, Route, Redirect } from 'react-router-dom';
 
-    this.state = {
-      dishes: DISHES,
-      promotions:PROMOTIONS,
-      leaders:LEADERS,
-      comments:COMMENTS
-    };
-  }
-  
-  onDishSelect(dish) {
-    this.setState({ selectedDish: dishId});
-  }
 
-  render() {
-    const HomePage=()=>{
-      RETURN (
-        <Home dish={this.state.dishes.filter((dish)=>dish.featured)[0]}
-        promotion={this.state.promotions.filter((promo)=>promo.featured)[0]}
-        leader={this.state.leaders.filter((leader)=>leader.featured)[0]}
-        />
-      );
+class Main extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            comments: COMMENTS,
+            dishes: DISHES,
+            leaders: LEADERS,
+            promotions: PROMOTIONS,
+        };
+
     }
 
+    render() {
 
-    const DishWithId=({match})=>{
-          return (
-            <DishDetail dish={this.state.dishes.filter((dish)=>dish.id===parseInt(match.params.dishId,10))[0]}
-            comments={this.state.comments.filter((comment)=>comment.dishId===parseInt(match.params.dishId,10))}
-            />
+        const HomePage = () => {
+            return(
+                <Home 
+                    dish={ this.state.dishes.filter( (dish)=>dish.featured )[0] }
+                    promotion={this.state.promotions.filter( (promotion)=>promotion.featured )[0] }
+                    leader={this.state.leaders.filter( (leader)=>leader.featured )[0] }
+                />
+            );
+        };
 
-          );
+        const AboutUsPage = () => {
+            return(
+                <About 
+                    leaders={this.state.leaders}
+                />
+            );
+        };
+
+
+        const DishWithId = ({match}) => {
+            return(
+                <DishDetail 
+                
+                dish={this.state.dishes.filter( (dish) => dish.id === parseInt(match.params.dishId, 10))[0] } 
+                comments={this.state.comments.filter( (comment) => comment.dishId === parseInt(match.params.dishId, 10)) } 
+                
+                
+                />
+            );
+        };
+
+
+
+        return (
+            <div>
+                <Header></Header>
+
+                <Switch>
+                    <Route path="/home" component={ HomePage } />
+                    <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/> }/>
+
+                    <Route path="/menu/:dishId" component={DishWithId} />
+
+                    <Route exact path="/contactus" component={Contact } />
+                    <Route exact path="/aboutus" component={ AboutUsPage } />
+                   
+                    {/* if url dosesnt match, bydefault redirect to */}
+                    <Redirect to="/home" />
+                </Switch>
+
+                <Footer></Footer>
+            </div> 
+        );
+
     }
-    return (
-      <div>
-        <Header />
-        <Switch>
-          <Route path="/home" component={HomePage}/>
-          <Route exact path="/menu" component={()=><Menu dishes={this.state.dishes}/> }/> 
-          <Route exact path="/contactus" component={contact}/>
-          <Route path="/menu/:dishId" component={DishWithId}/>
-          <Redirect to="/home"/>
-          </Switch>
-      <Footer />
-      </div>
 
-    );
-  }
 }
 
 export default Main;
