c92ae1f4dc6d9df7a1e3a3c227071a535bb44e5a
Vanshika <vanshipahwa99@gmail.com>
2021-09-03 14:15:59 +0530

    react-redux forms revisited

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import DishDetail from './DishdetailComponent'
 import { Switch, Route, Redirect,withRouter } from 'react-router-dom';
 import {connect} from 'react-redux';
 import {addComment,fetchDishes} from '../redux/ActionCreators';
+import { actions } from 'react-redux-fom';
 
 const mapStateToProps=state=>{
     return{
@@ -23,7 +24,8 @@ const mapStateToProps=state=>{
 
 const mapDispatchToProps=(dispatch)=>({
 addComment:(dishId,rating,author,comment)=>dispatch(addComment(dishId,rating,author,comment))
-,fetchDishes:()=>{dispatch(fetchDishes())}
+,fetchDishes:()=>{dispatch(fetchDishes())},
+resetFeedbackForm:()=>{ dispatch(actions.reset('feedback'))}
 });
 
 class Main extends Component {
@@ -83,7 +85,7 @@ componentDidMount(){
 
                     <Route path="/menu/:dishId" component={DishWithId} />
 
-                    <Route exact path="/contactus" component={Contact } />
+                    <Route exact path="/contactus" component={()=><Contact resetFeedbackForm={this.props.resetFeedbackForm}/>} />
                     <Route exact path="/aboutus" component={ AboutUsPage } />
                    
                     {/* if url dosesnt match, bydefault redirect to */}
